869a46408b0b695bca1d64fe5951b8c9df6117ea
esayan <esayan@gmail.com>
2025-11-03 22:13:48 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,9 +26,7 @@ export default function App() {
         <div className="px-6 sm:px-16">
           <div className="max-w-7xl mx-auto relative">
             {/* gradient lekeler */}
-            <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
-            <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
-            <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+
             <Hero />
           </div>
         </div>
