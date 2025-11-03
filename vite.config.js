001e558176fa111bb798eb64e833c1c646bc0043
esayan <esayan@gmail.com>
2025-11-03 21:22:10 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/inno-craft-site/',   // <-- repo adın
+  base: '/inno-craft-site/', // repo adın
   plugins: [react()],
 })
