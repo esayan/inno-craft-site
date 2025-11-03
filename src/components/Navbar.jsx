d172ae4993967fac5308ac0d4e72b8a9476b5355
esayan <esayan@gmail.com>
2025-11-03 22:10:12 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,43 +2,63 @@ import React, { useState } from 'react'
 import { close, logo, menu } from '../assets'
 import { navLinks } from '../constants'
 
-const Navbar = () => {
+export default function Navbar() {
+  const [open, setOpen] = useState(false)
 
-  const [toggle, setToggle] = useState(false)
+  const handleToggle = () => setOpen(prev => !prev)
+  const handleClose = () => setOpen(false)
 
   return (
-    <nav className='w-full flex py-6 justify-between items-center navbar'>
-      <img src={logo} alt='hoobank' className='w-[124px] h-[32px]'/>
-      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+    <nav className="w-full flex py-6 justify-between items-center navbar relative z-50">
+      <a href="#home" className="inline-flex items-center gap-2">
+        <img src={logo} alt="Inno Craft" className="w-[124px] h-[32px]" />
+      </a>
+
+      {/* Desktop menu */}
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, i) => (
-          <li 
+          <li
             key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] ${i === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white mr-10`}
+            className={`font-poppins font-medium cursor-pointer text-[16px] ${
+              i === navLinks.length - 1 ? 'mr-0' : 'mr-10'
+            } text-dimWhite hover:text-white transition-colors`}
           >
-            <a href={`#${nav.id}`}>
-              {nav.title}
-            </a>
-          </li>        
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
         ))}
       </ul>
-      <div className='sm:hidden flex flex-1 justify-end items-center'>
-        <img
-          src={toggle ? close : menu}
-          alt='menu'
-          className='w-[28px] h-[28px] object-contain'
-          onClick={() => setToggle((previous) => !previous)}
-        />
-        <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
-          <ul className='list-none flex flex-col justify-end items-center flex-1'>
-            {navLinks.map((nav, i) => (
-              <li 
+
+      {/* Mobile toggle */}
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <button
+          type="button"
+          aria-label="Toggle menu"
+          onClick={handleToggle}
+          className="p-2 rounded-lg hover:opacity-90 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/40"
+        >
+          <img
+            src={open ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain"
+          />
+        </button>
+
+        {/* Mobile dropdown */}
+        <div
+          className={`${
+            open ? 'flex' : 'hidden'
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[180px] rounded-xl sidebar z-50 backdrop-blur-sm ring-1 ring-white/10`}
+        >
+          <ul className="list-none flex flex-col items-start gap-4">
+            {navLinks.map((nav) => (
+              <li
                 key={nav.id}
-                className={`font-poppins font-normal cursor-pointer text-[16px] ${i === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white mr-10`}
+                className="font-poppins font-medium cursor-pointer text-[16px] text-dimWhite hover:text-white transition-colors"
               >
-                <a href={`#${nav.id}`}>
+                <a href={`#${nav.id}`} onClick={handleClose}>
                   {nav.title}
                 </a>
-              </li>        
+              </li>
             ))}
           </ul>
         </div>
@@ -46,5 +66,3 @@ const Navbar = () => {
     </nav>
   )
 }
-
-export default Navbar
